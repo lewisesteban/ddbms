bf431120da8aed2e4fb6162223733fbc13929ae0
Emile Jonas <emile.jonas@epitech.eu>
2017-12-26 14:40:52 +0900

    modified ressources for Article controller in order for them to be loaded by Spring's default Ressource resolver

---
+++ src/main/java/ddbms/ArticleController.java
@@ -0,0 +1,17 @@
+package ddbms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ArticleController {
+
+    @RequestMapping("/article")
+    public String article(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "article";
+    }
+
+}
