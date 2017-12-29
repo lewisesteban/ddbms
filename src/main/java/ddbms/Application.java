99fd4c9a03d89ef07c523b006c2d933dfb8800dc
Emile Jonas <emile.jonas@epitech.eu>
2017-12-29 14:12:20 +0900

    S3 bucket, fileupload

--- src/main/java/ddbms/Application.java
+++ src/main/java/ddbms/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
     public static void main(String[] args) {
@@ -12,4 +13,11 @@ public class Application {
         //Connector.get();
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+      return (args) -> {
+          storageService.deleteAll();
+          storageService.init();
+        };
+    }
 }
